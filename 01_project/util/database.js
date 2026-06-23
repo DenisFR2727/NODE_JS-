a8be6df2f90bcdbf27adff1a9a76953ad01d9cc6
Denis Leon <dh92fr@ukr.net>
2026-06-23 14:53:45 +0300

    delete commit in  util/database.js

--- 01_project/util/database.js
+++ 01_project/util/database.js
@@ -29,26 +29,3 @@ const getDb = () => {
   throw "No database found!";
 };
 module.exports = { mongoConnect, getDb };
-
-// Sequelize version
-// const Sequelize = require("sequelize");
-
-// const sequelize = new Sequelize("test_sql", "root", "Specialized8110", {
-//   dialect: "mysql",
-//   host: "localhost",
-// });
-
-// module.exports = sequelize;
-
-// MySQL version poll
-
-// const mysql = require("mysql2");
-
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "test_sql",
-//   password: "Specialized8110",
-// });
-
-// module.exports = pool.promise();
